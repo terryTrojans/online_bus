258ddea0104b799ba461f2ef133087244d6d2dd1
Kagisho Terry <terrypacers@gmail.com>
2023-08-01 10:58:07 +0200

    commit number 5 to the online bus registration application...

--- dashboard.php
+++ dashboard.php
@@ -5,6 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 session_start();
 if(!isset($_SESSION['admin_id'])) {
     header("Location: index.php");
